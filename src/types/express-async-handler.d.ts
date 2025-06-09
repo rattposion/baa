aeeb6bd2443be344bd53b823c2e1ada4b5b643f5
Wesley <142677140+rattposion@users.noreply.github.com>
2025-06-09 17:07:27 -0300

    Delete src directory

--- src/types/express-async-handler.d.ts
+++
@@ -1,6 +0,0 @@
-import { RequestHandler } from 'express';
-
-declare module 'express-async-handler' {
-  function expressAsyncHandler(handler: RequestHandler): RequestHandler;
-  export = expressAsyncHandler;
-} 
