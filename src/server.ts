df4123af01e9e0701a274de3af861c0cb6adb978
Wesley <142677140+rattposion@users.noreply.github.com>
2025-07-16 14:16:58 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
-  'https://decore-gest-o-de-produ-o.vercel.app'
+  'https://decore-eight.vercel.app'
 ];
 
 if (process.env.CORS_ORIGIN) {
